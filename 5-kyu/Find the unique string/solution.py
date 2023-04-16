da36875fe33ad263a965b74fe374dc9981be9383
Nikolas <ZaL1Vv2003@gmail.com>
2023-04-16 23:08:06 +0200

    corrects kata 5-kyu Find the unique string

--- 5-kyu/Find the unique string/solution.py
+++ 5-kyu/Find the unique string/solution.py
@@ -2,7 +2,9 @@ def find_uniq(arr):
     arr_list = []
     for i in arr:
         arr_list.append(set(i.lower()))
-    unique = [i for i in arr_list if arr_list.count(i) == 1][0]
+    for i in arr_list:
+        if arr_list.count(i) == 1:
+            unique = i
     result = arr_list.index(unique)
     return arr[result]
 

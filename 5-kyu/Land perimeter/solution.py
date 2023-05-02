5b64cf7908c0d34c97c69b8f0ef6aca44e5e5c47
Nikolas <ZaL1Vv2003@gmail.com>
2023-05-02 18:02:50 +0200

    updates 5-kyu Land perimeter

--- 5-kyu/Land perimeter/solution.py
+++ 5-kyu/Land perimeter/solution.py
@@ -1,20 +1,15 @@
 def land_perimeter(arr):
     perimeter = 0
-    storage = ''
-    box1 = ''
-    box2 = ''
-    for item in arr:
-        box1 = [i for i, c in enumerate(item) if c == 'X']
-        for bx1 in box1:
-            for bx2 in box2:
-                if bx1 == bx2:
-                    perimeter -= 2
-        box2 = [i for i, c in enumerate(item) if c == 'X']
-        for i in item:
-            if i == 'X':
-                if i == storage:
-                    perimeter -= 2
+    rows = len(arr)
+    cols = len(arr[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if arr[i][j] == 'X':
                 perimeter += 4
-            storage = i
-        storage = ''
+                if i > 0 and arr[i - 1][j] == 'X':
+                    perimeter -= 2
+                if j > 0 and arr[i][j - 1] == 'X':
+                    perimeter -= 2
+
     return f"Total land perimeter: {perimeter}"
